db5bb63942b33dc9a8a31a3d2cc6b892669ecb16
DAZ <daz4126@gmail.com>
2009-09-07 07:43:07 +0100

    removed reference to helpers file

--- main.rb
+++ main.rb
@@ -10,7 +10,6 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/test.db"
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/lib")
 require 'auth'
 require 'page'
-require 'helpers'
 
 DataMapper.auto_upgrade!
 
